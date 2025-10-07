effa79918d90de07f678a9e3fa47fe3e5f14b319
varshar26 <varshareddy@berkeley.edu>
2025-10-07 10:21:56 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .catch(err => console.error(err));
